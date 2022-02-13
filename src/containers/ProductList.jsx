dcc0b3a38b382e61508905e56271a292bf54f981
AyrielNoriega <anoriega2310@gmail.com>
2022-02-13 16:54:05 -0500

    routes && others

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { ProductItem } from '@components/ProductItem';
+import axios from 'axios';
+import '@styles/ProductList.scss';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
+
+export const ProductList = () => {
+	const [products, setProducts] = useState([]);
+ 
+	useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response.data);
+	}, [])
+ 
+	return (
+		<section className="main-container">
+			<div className="ProductList">
+				<ProductItem />
+			</div>
+		</section>
+	);
+}
